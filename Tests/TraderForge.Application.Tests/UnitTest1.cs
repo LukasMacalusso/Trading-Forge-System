c140c5817a54e4325f8b96eb40813cecec688e44
Ignacio Loncón <ignacioloncon2006@gmail.com>
2026-05-09 13:42:24 -0400

    projects added for the tests

---
+++ Tests/TraderForge.Application.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace TraderForge.Application.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Tests/TraderForge.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace TraderForge.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
