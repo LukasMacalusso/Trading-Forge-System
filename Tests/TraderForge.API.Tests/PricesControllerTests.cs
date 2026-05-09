de42ab06b62931ba532529f99bb142ac51d362fc
Ignacio Loncón <ignacioloncon2006@gmail.com>
2026-05-09 17:53:06 -0400

    unit test in API layer

---
+++ Tests/TraderForge.API.Tests/PricesControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TraderForge.API.Controllers;
+using TraderForge.API.Requests;
+using TraderForge.Application.Handlers;
+using TraderForge.Domain.Services;
+namespace TraderForge.API.Tests;
+
+public class PricesControllerTests
+{
+    private readonly PricesController _controller;
+    private readonly Mock<IMarketService> _marketServiceMock;
+    public PricesControllerTests()
+    {
+        _marketServiceMock = new Mock<IMarketService>();
+        var handler = new GetMarketPricesQueryHandler(_marketServiceMock.Object);
+        _controller = new PricesController(handler);
+    }
+
+    [Fact]
+    public async Task GetPrices_WhenSymbolsListIsEmpty_ReturnsBadRequest()
+    {
+        var request = new GetMarketPricesRequest { Symbols = [] };
+        var result = await _controller.GetPrices(request);
+        Assert.IsType<BadRequestObjectResult>(result);
+    }
+}
