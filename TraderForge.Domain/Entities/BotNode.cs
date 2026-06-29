65909e3b9e7d52015c76ff7631a975d28ec8b632
Ignacio Loncón <ignacioloncon2006@gmail.com>
2026-06-29 14:11:29 -0400

    new system

--- TraderForge.Domain/Entities/BotNode.cs
+++ TraderForge.Domain/Entities/BotNode.cs
@@ -14,12 +14,9 @@ public class BotNode
     public double PositionY { get; private set; }
     public bool IsActive { get; private set; }
     public DateTime CreatedAt { get; private set; }
-
     public ICollection<BotEdge> OutgoingEdges { get; private set; } = new List<BotEdge>();
     public ICollection<BotEdge> IncomingEdges { get; private set; } = new List<BotEdge>();
-
     private BotNode() { }
-
     public BotNode(Guid strategyId, BotNodeType type, string name, string config, double posX, double posY)
     {
         Id = Guid.NewGuid();
